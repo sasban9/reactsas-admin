12bbb67ff692c2acb052e29d615fdd0d28934a4a
sasban <azure.saswata@gmail.com>
2022-12-12 15:12:56 +0530

    Search box & icons in Topbar added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import { ColorModeContext, useMode } from "./theme";
-import { CssBaseline, ThemeProvider, ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import Topbar from "./scenes/global/Topbar";
 
 function App() {
